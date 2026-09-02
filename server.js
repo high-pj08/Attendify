bde301f90bdf8d2696f538207a453782ecbe803d
Prince <jhap36083@gmail.com>
2026-09-03 00:06:11 +0530

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
 
-        app.listen(process.env.PORT, () => {
-            console.log(
-                `Server running on http://localhost:${process.env.PORT}`
-            );
+        const PORT = process.env.PORT || 5000;
+
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
         });
-    })
+       })
     .catch((error) => {
         console.log(
             "MongoDB connection failed:",
